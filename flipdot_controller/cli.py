c480a5252d21b3ba957856e854630aa0fc0c8c7c
briggySmalls <wasso14@hotmail.com>
2019-02-11 00:57:16 +0000

    Update CLI usage

--- flipdot_controller/cli.py
+++ flipdot_controller/cli.py
@@ -9,18 +9,22 @@ from flipdot_controller.controller import FlipdotController, SignConfig
 from flipdot_controller.power import PinConfig
 from flipdot_controller.server import Server
 
-SIGNS = [SignConfig(name="top", address=1, width=84, height=7, flip=True)]
+SIGNS = [
+    SignConfig(name="top", address=1, width=84, height=7, flip=True),
+    SignConfig(name="bottom", address=2, width=84, height=7, flip=False),
+]
 
 PINS = PinConfig(sign=38, light=40)
 
 
 @click.command()
-@click.option('--port', help="Name of serial port")
-def main(port):
+@click.option('--serial-port', help="Name of serial port")
+@click.option('--grpc-port', type=int, help="Number of gRPC port")
+def main(serial_port, grpc_port):
     """Console script for flipdot_controller."""
-    with Serial(port) as ser, FlipdotController(
+    with Serial(serial_port) as ser, FlipdotController(
             port=ser, signs=SIGNS, pins=PINS) as controller:
-        server = Server(controller)
+        server = Server(controller, port=grpc_port)
         try:
             server.start()
         except KeyboardInterrupt:
