32ed49a43161452028037eb871af22bcec77f5ba
briggySmalls <wasso14@hotmail.com>
2019-04-15 09:04:01 +0100

    Rework tests with beforeEach

--- web/tests/unit/login.spec.ts
+++ web/tests/unit/login.spec.ts
@@ -1,16 +1,40 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, Wrapper } from '@vue/test-utils';
+import { expect } from 'chai';
 import Login from '@/components/Login.vue';
 import { Client } from '@/ts/client';
 import { Interpreter } from 'xstate/lib/interpreter';
 import { mock, instance, when, anything, verify } from 'ts-mockito';
-
-const mockedFsm = mock(Interpreter);
+import { CombinedVueInstance, Vue } from 'vue/types/vue';
+import { grpc } from '@improbable-eng/grpc-web';
+import { StateSchema, EventObject } from 'xstate';
 
 describe('Login.vue', () => {
-  it('authenticates', () => {
+  let mockedClient: Client;
+  let client: Client;
+
+  let mockedFsm: Interpreter<{}, StateSchema, EventObject>;
+  let fsm: Interpreter<{}, StateSchema, EventObject>;
+
+  let wrapper: Wrapper<CombinedVueInstance<Login, object, object, object, Record<never, any>>>;
+
+  beforeEach(() => {
     // Create a mock client
-    const mockedClient = mock(Client);
-    const client = instance(mockedClient);
+    mockedClient = mock(Client);
+    client = instance(mockedClient);
+    // Create a mock fsm
+    mockedFsm = mock(Interpreter);
+    fsm = instance(mockedFsm);
+    // Create object under test
+    wrapper = shallowMount(Login, {
+      propsData: {
+        client,
+        fsm,
+      },
+    });
+  });
+
+  it('authenticates', () => {
+    // Configure client
     when(mockedClient.error).thenReturn(null);
     when(mockedClient.authenticate('password', anything())).thenCall(
         (password: string, callback: (response: any) => void) => {
@@ -18,20 +42,34 @@ describe('Login.vue', () => {
             callback(null);
         },
     );
-    // Create a mock fsm
-    const fsm = instance(mockedFsm);
-    // Create the component
-    const wrapper = shallowMount(Login, {
-        propsData: {
-            client,
-            fsm,
-        },
-    });
     // Send password
     wrapper.find('#login-password').setValue('password');
     wrapper.find('#login-submit').trigger('submit');
     // Set a password
     verify(mockedClient.authenticate('password', anything())).once();
     verify(mockedFsm.send('AUTH')).once();
+  });
+
+  it('displays error', () => {
+    // // Configure client
+    when(mockedClient.error).thenReturn(null);
+    when(mockedClient.authenticate('password', anything())).thenCall(
+      (password: string, callback: (response: any) => void) => {
+        // Indicate an error occurred
+        when(mockedClient.error).thenReturn({
+          code: grpc.Code.Unauthenticated,
+          message: 'Failed to login',
+        });
+        // Execute callback
+        callback(null);
+      },
+    );
+    // Send password
+    wrapper.find('#login-password').setValue('password');
+    wrapper.find('#login-submit').trigger('submit');
+    // Set a password
+    verify(mockedClient.authenticate('password', anything())).once();
+    verify(mockedFsm.send(anything())).never();
+    expect(wrapper.find('#login .alert').isVisible());
   });
 });
